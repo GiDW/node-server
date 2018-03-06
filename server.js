085a83d2d1173da6cc3664f4e92abb310b2ef4ed
Gilles De Waele <de.waele.gilles@gmail.com>
2018-03-07 00:54:24 +0100

    Added basic argument support

--- server.js
+++ server.js
@@ -2,8 +2,57 @@
 
 'use strict';
 
-var Server = require('./lib/libserver');
+var Server = require('./lib/server');
 
-var srv = new Server();
+var srv = new Server(processArgs());
 
 srv.start();
+
+function processArgs () {
+
+    var i, length, arg, options;
+
+    options = {};
+
+    length = process.argv.length;
+    for (i = 0; i < length; i++) {
+
+        arg = process.argv[i];
+
+        if (arg.indexOf('-port=') === 0) {
+
+            options.port = parseInt(arg.substring(6), 10);
+
+        } else if (arg.indexOf('-cors=') === 0) {
+
+            options.cors = arg.substring(6);
+
+        } else if (arg.indexOf('-logRequest') === 0) {
+
+            if (arg === '-logRequest' ||
+                arg === '-logRequest=true' ||
+                arg === '-logRequest=1') {
+
+                options.logRequest = true;
+
+            } else if (arg === '-logRequest=false' ||
+                arg === '-logRequest=0') {
+
+                options.logRequest = false;
+
+            } else if (arg === '-logRequestHeaders' ||
+                arg === '-logRequestHeaders=true' ||
+                arg === '-logRequestHeaders=1') {
+
+                options.logRequestHeaders = true;
+
+            } else if (arg === '-logRequestHeaders=false' ||
+                arg === '-logRequestHeaders=0') {
+
+                options.logRequestHeaders - false;
+            }
+        }
+    }
+
+    return options;
+}
