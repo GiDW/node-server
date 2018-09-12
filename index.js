bc2adfca74e6abd5f8410994eb3e19fd8124b6eb
Gilles De Waele <de.waele.gilles@gmail.com>
2018-09-12 10:49:18 +0200

    Convreted to standard style

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-var Server = require('./lib/server');
+var Server = require('./lib/server')
 
-new Server(processArgs()).start();
+new Server(processArgs()).start()
 
 function processArgs () {
+  var i, length, arg, options
 
-    var i, length, arg, options;
+  options = {}
 
-    options = {};
+  length = process.argv.length
+  for (i = 0; i < length; i++) {
+    arg = process.argv[i]
 
-    length = process.argv.length;
-    for (i = 0; i < length; i++) {
-
-        arg = process.argv[i];
-
-        if (arg.indexOf('-port=') === 0) {
-
-            options.port = parseInt(arg.substring(6), 10);
-
-        } else if (arg.indexOf('-cors=') === 0) {
-
-            options.cors = arg.substring(6);
-
-        } else if (arg.indexOf('-logRequest') === 0) {
-
-            if (arg === '-logRequest' ||
-                arg === '-logRequest=true' ||
-                arg === '-logRequest=1') {
-
-                options.logRequest = true;
-
-            } else if (arg === '-logRequest=false' ||
-                arg === '-logRequest=0') {
-
-                options.logRequest = false;
-
-            } else if (arg === '-logRequestHeaders' ||
-                arg === '-logRequestHeaders=true' ||
-                arg === '-logRequestHeaders=1') {
-
-                options.logRequestHeaders = true;
-
-            } else if (arg === '-logRequestHeaders=false' ||
-                arg === '-logRequestHeaders=0') {
-
-                options.logRequestHeaders - false;
-            }
-
-        } else if (arg.indexOf('-rootLookForIndex') === 0) {
-
-            options.rootLookForIndex =
-                arg === '-rootLookForIndex' ||
-                arg === '-rootLookForIndex=true' ||
-                arg === '-rootLookForIndex=true';
-        }
+    if (arg.indexOf('-port=') === 0) {
+      options.port = parseInt(arg.substring(6), 10)
+    } else if (arg.indexOf('--port=') === 0) {
+      options.port = parseInt(arg.substring(7), 10)
+    } else if (arg.indexOf('-p=') === 0) {
+      options.port = parseInt(arg.substring(3), 10)
+    } else if (arg.indexOf('-cors=') === 0) {
+      options.cors = arg.substring(6)
+    } else if (arg.indexOf('-logRequest') === 0) {
+      if (arg === '-logRequest' ||
+        arg === '-logRequest=true' ||
+        arg === '-logRequest=1') {
+        options.logRequest = true
+      } else if (arg === '-logRequest=false' ||
+        arg === '-logRequest=0') {
+        options.logRequest = false
+      } else if (arg === '-logRequestHeaders' ||
+        arg === '-logRequestHeaders=true' ||
+        arg === '-logRequestHeaders=1') {
+        options.logRequestHeaders = true
+      } else if (arg === '-logRequestHeaders=false' ||
+        arg === '-logRequestHeaders=0') {
+        options.logRequestHeaders = false
+      }
+    } else if (arg.indexOf('-rootLookForIndex') === 0) {
+      options.rootLookForIndex =
+        arg === '-rootLookForIndex' ||
+        arg === '-rootLookForIndex=true' ||
+        arg === '-rootLookForIndex=true'
     }
+  }
 
-    return options;
+  return options
 }
