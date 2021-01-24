240f0237fc1dfb9ae2673484c472b064a9f9928d
Gilles De Waele <de.waele.gilles@gmail.com>
2021-01-24 12:08:47 +0100

    Bump dependencies + cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-  extends: 'standard',
+  extends: '@gidw/eslint-config-standard-node',
   parserOptions: {
     ecmaVersion: 5,
     sourceType: 'script'
   },
   env: {
     node: true
-  }
+  },
+  ignorePatterns: [
+    'node_modules/'
+  ]
 }
